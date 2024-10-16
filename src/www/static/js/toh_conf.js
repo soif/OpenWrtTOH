4bb96899cd253a0966773cfb07ff866e82e86be5
soif <wxopwx@gmail.com>
2024-10-16 11:04:23 +0200

    split own css and js to separate files

---
+++ src/www/static/js/toh_conf.js
@@ -0,0 +1,298 @@
+
+// Config ########################################################################
+let owrtUrls={
+	www: 		"https://openwrt.org/",
+	toh_json:	"https://openwrt.org/",
+	media:		"https://openwrt.org/_media/",
+}
+
+function FormatterLink(cell, formatterParams, onRendered) {
+	var value = cell.getValue();
+	if (value && value.length > 0) {
+	return "<a href='" + value + "' target='_blank'>" + formatterParams.label + "</a>";
+	} 
+	return "";
+}
+function FormatterLinkDevice(cell, formatterParams, onRendered) {
+	var value = cell.getValue();
+	if (value && value.length > 0) {
+		value=value.replace(/:/g,'/');
+		return "<a href='" + owrtUrls.www + value + "' target='_blank'>TOH</a>";
+	} 
+	return "";
+}
+function FormatterImages(cell, formatterParams, onRendered) {
+	var arr = cell.getValue();
+	var url='';
+	var out='';
+	var label='';
+	if (Array.isArray(arr) && arr.length > 0) {
+		arr.forEach((value, index) => {
+			if(value.match(/^http/)){
+				url=value;
+			}
+			else{
+				value=value.replace(/:/g,'/');
+				url=owrtUrls.media + value;
+			}
+			if(value.match(/genericrouter1.png$/)){
+				label='<i class="fa-regular fa-image generic"></i>';
+			}
+			else{
+				label='<i class="fa-solid fa-image"></i>';
+			}
+			out +='<a href="' + url + '" target="_blank" class="cell-image" title="'+url+'">'+label+'</a> ';
+		});
+		return out;
+	} 
+	return arr;
+}
+function FormatterCleanEmpty(cell, formatterParams, onRendered) {
+	var value = cell.getValue();
+	if (value && value.length > 0) {
+		value=value.replace(/-/g,'');
+		return value;
+	} 
+	return "";
+}
+function FormatterFlash(cell, formatterParams, onRendered) {
+	var arr = cell.getValue();
+	var out='';
+	var done=false;
+	if (Array.isArray(arr) && arr.length > 0) {
+		arr.forEach((value, index) => {
+			value=value.replace(/NAND/g,' NAND');
+			if(done){
+				out +="+";
+			}
+			out +=value;
+			done=true;
+		});
+		return out;
+	}
+	return arr;
+}
+function FormatterYesNo(cell, formatterParams, onRendered) {
+	var value = cell.getValue();
+	var icon='';
+	if (typeof value === "string") {
+		value=value.toLowerCase().trim();
+	}
+	if(value=='yes'){
+		icon='fa-solid fa-check';
+	}
+	else if(value=='no'){
+		icon='fa-solid fa-xmark';
+	}
+	else if(value=='-'){
+		icon='fa-solid fa-question dash';
+	}
+	else if(value==''){
+		icon='fa-solid fa-question empty';		
+	}
+	else{
+		icon='fa-solid fa-question unknown';
+	}
+	return '<i class="'+icon+'"></i>';
+}
+
+let columnStyles={
+	brand: {frozen:true},
+	model: {frozen:true},
+	devicepage:							{title:"Device Page",		formatter: FormatterLinkDevice, tooltip:false },
+	ethernet100mports: 					{title: "Eth 100", 			formatter: FormatterCleanEmpty},
+	ethernet1gports: 					{title: "Eth 1G", 			formatter: FormatterCleanEmpty},
+	ethernet2_5gports: 					{title: "Eth 2.5G", 		formatter: FormatterCleanEmpty},
+	ethernet5gports: 					{title: "Eth 5G", 			formatter: FormatterCleanEmpty},
+	ethernet10gports: 					{title: "Eth 10G", 			formatter: FormatterCleanEmpty},
+	fccid:								{title:"FCC Id",			formatter: FormatterLink, formatterParams:{ label:'FCC Id'} },
+	firmwareoemstockurl:				{title:"Stock Firm.",		formatter: FormatterLink, formatterParams:{ label:'Stock'} },
+	firmwareopenwrtinstallurl:			{title:"Owrt Install",		formatter: FormatterLink, formatterParams:{ label:'Install'} },
+	firmwareopenwrtupgradeurl:			{title:"Owrt Upgrade",		formatter: FormatterLink, formatterParams:{ label:'Upgrade'} },
+	firmwareopenwrtsnapshotinstallurl:	{title:"Owrt Snap.Inst.",	formatter: FormatterLink, formatterParams:{ label:'Snap.Install'} },
+	firmwareopenwrtsnapshotupgradeurl:	{title:"Owrt Snap.Upgr.",	formatter: FormatterLink, formatterParams:{ label:'Snap.Upgrade'} },
+	flashmb:							{title:"Flash (Mb)",		formatter: FormatterFlash, sorter: 'array', sorterParams: {type:"min", alignEmptyValues:"bottom"} },
+	oemdevicehomepageurl:				{title:"OEM Page",			formatter: FormatterLink, formatterParams:{ label:'OEM'} },
+	outdoor:							{title:"OutDoor",			formatter: FormatterYesNo, sorter: 'string' },
+	owrt_forum_topic_url:				{title:"Forum",				formatter: FormatterLink, formatterParams:{ label:'Forum'} },
+	picture:							{title:"Image",				formatter: FormatterImages, sorter: 'array' },
+	wikideviurl:						{title:"Wiki",				formatter: FormatterLink, formatterParams:{ label:'Wiki'} },
+};
+
+// let columnOrder=[
+// 	'brand',
+// 	'model',
+// 	'cpu',
+// 	'rammb',
+// 	'flashmb',
+// ];
+let columnOrder=[];
+
+
+
+let colViewGroups={
+	base:{
+		name: 'Main',
+		fields:[
+			'brand',
+			'model',
+		]
+	},
+	
+	hardware_main:{
+		name: 'Hardware',
+		fields:[
+			'cpu',
+			'rammb',
+			'flashmb',
+			'cpucores',
+			'cpumhz',
+			'switch',
+		]
+	},
+
+	hardware_ports:{
+		name: 'Hardware Ports',
+		fields:[
+			'bluetooth',
+			'buttoncount',
+			'detachableantennas',
+			'gpios',
+			'jtag',
+			'ledcount',
+			'modem',
+			'outdoor',
+			'phoneports',
+			'powersupply',
+			'sataports',
+			'usbports',
+			'commentsusbsataports',
+			'serial',
+			'serialconnectionvoltage',
+			'audioports',
+			'videoports',
+			'commentsavports',
+		]
+	},
+
+	network:{
+		name: 'Network',
+		fields:[
+			'ethernet100mports',
+			'ethernet1gports',
+			'ethernet2_5gports',
+			'ethernet5gports',
+			'ethernet10gports',
+			'sfp_ports',
+			'sfp_plus_ports',
+			'vlan',
+			'commentsnetworkports',
+		]
+	},
+
+	openwrt:{
+		name: 'OpenWRT',
+		fields:[
+			'deviceid',
+			'version',
+			'target',
+			'subtarget',
+			'gitsearch',
+			'supportedcurrentrel',
+			'supportedsincecommit',
+			'supportedsincerel',
+			'installationmethods',
+			'commentinstallation',
+			'unsupported_functions',
+		]
+	},
+
+
+	software:{
+		name: 'Software',
+		fields:[
+			'bootloader',
+			'packagearchitecture',
+			'wlandriver',
+			'recoverymethods',
+			'commentrecovery',
+			'serialconnectionparameters',
+		]
+	},
+
+	links:{
+		name: 'Links',
+		fields:[
+			'devicepage',
+			'firmwareopenwrtinstallurl',
+			'firmwareopenwrtupgradeurl',
+			'firmwareopenwrtsnapshotinstallurl',
+			'firmwareopenwrtsnapshotupgradeurl',
+			'firmwareoemstockurl',
+			'forumsearch',
+			'oemdevicehomepageurl',
+			'owrt_forum_topic_url',
+			'wikideviurl',
+			'fccid',
+		]
+	},
+
+	misc:{
+		name: 'Misc',
+		fields:[
+			'devicetype',			
+			'availability',
+			'picture',			
+			'whereavailable',			
+			'comments',			
+		]
+	},
+
+	wifi:{
+		name: 'Wifi',
+		fields:[
+			'wlan24ghz',
+			'wlan50ghz',
+			'wlan60ghz',
+			'wlan600ghz',
+			'wlanhardware',
+			'wlancomments',
+		]
+	},
+};
+
+let colViews={
+	normal:	[
+		...colViewGroups.base.fields,
+		...colViewGroups.hardware_main.fields,
+		...colViewGroups.links.fields
+	],
+	mini:	['brand','model','cpu','rammb','flashmb','devicepage'],
+
+	hardware:	[
+		...colViewGroups.base.fields,
+		...colViewGroups.hardware_main.fields,
+		...colViewGroups.hardware_ports.fields,
+	],
+	network:	[
+		...colViewGroups.base.fields,
+		...colViewGroups.network.fields,
+	],
+	software:	[
+		...colViewGroups.base.fields,
+		...colViewGroups.software.fields,
+	],
+	links:	[
+		...colViewGroups.base.fields,
+		...colViewGroups.links.fields,
+	],
+	misc:	[
+		...colViewGroups.base.fields,
+		...colViewGroups.misc.fields,
+	],
+};
+
+let prefs={
+	def_view: 'mini'
+};
+
---
+++ src/www/static/js/toh_main.js
@@ -0,0 +1,373 @@
+
+
+// ############################################################################################################
+$(document).ready(function () {
+
+	// Get parameters
+	function getUrlParameter(name) {
+		name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+		var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+		var results = regex.exec(location.search);
+		return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+	}
+	// ------------------------------------------
+	function getParameterOrDefault(name, defaultValue) {
+		var value = getUrlParameter(name);
+		return value !== '' ? value : defaultValue;
+	}
+
+
+
+
+	// ------------------------------------------
+	function htmlViewLine(field,title,checked){
+		let html='';
+		html +='<li><input type="checkbox" value="'+field+'"';
+		if( checked ){html +=' checked="true"';} 
+		html +='> '+title+' <small>('+field+")</small>\n";
+		return html;
+	}
+	// ------------------------------------------
+	function htmlViewGroup(title,group){
+		let html='';
+		html +='<div class="view-group" data-group="'+group+'">'+"\n"+'<div class="group-title"><a href="#" class="view-link" title="Toggle group visibility"><i class="fa-solid fa-square"></i> '+title+'</a></div>'+"\n";
+		html +='<ul>'+"\n";
+		return html;
+	}
+		
+	// ------------------------------------------
+	function displayCustomColumns(){
+		let columns = table.getColumnDefinitions();  
+		let view="";
+		let col={};
+		$.each(colViewGroups,function(key,arr){
+			view +=htmlViewGroup(arr.name,key);
+			$.each(arr.fields,function(k,field){
+				col=table.getColumn(field);
+				view +=htmlViewLine(field, col.getDefinition().title, col.isVisible())
+				//remove from colums
+				const index = columns.findIndex(item => item.field === field);
+				if (index !== -1) {columns.splice(index, 1)[0];}
+			});
+			view +="</ul>\n</div>\n";
+		});
+		if(columns.length > 0){
+			view +=htmlViewGroup('Unsorted','unsorted');
+			$.each(columns,function(key,arr){
+				col=table.getColumn(arr.field);
+				view +=htmlViewLine(arr.field, arr.title, col.isVisible())
+			});
+			view +="</ul>\n</div>\n";
+		}
+		$("#custom-content").html(view);
+		groupsUpdateIcon();
+
+	}
+
+	// ------------------------------------------
+	function groupsUpdateIcon(){
+		$('.view-group').each(function(i){
+			var total=$(this).find('LI').length;
+			var checked=$(this).find('LI INPUT:checked').length;
+			var icon ="";
+			if(checked==total){
+				icon='fa-regular fa-square-check';
+			}
+			else if(checked==0){
+				icon='fa-regular fa-square';
+			}
+			else{
+				icon='fa-regular fa-square-minus';
+			}
+			$(this).find('.group-title I').removeClass().addClass(icon);
+		});
+	}
+
+
+	// ------------------------------------------
+	function applyView(key) {
+		$('#head-loading').show();
+		setTimeout(function(){
+			//table.blockRedraw();
+			if(key=='all'){
+				showAllColumns(true);
+			}
+			else if(key=='none'){
+				showAllColumns(false);
+			}
+			else{
+				const arr = colViews[key];
+				if (arr) {
+					showAllColumns(false);
+					arr.forEach(col => table.showColumn(col));
+				}
+			}
+			displayCustomColumns();
+			//table.redraw(true).then(hideLoading);
+			//table.restoreRedraw();
+		},0);
+	}
+
+	// ------------------------------------------
+	function showAllColumns(bool) {
+		var columnDefs = table.getColumnDefinitions();  
+		columnDefs.forEach(function(column) {
+			if(bool){
+				table.showColumn(column.field);
+			}
+			else{
+				table.hideColumn(column.field);
+			}
+		});
+	}
+
+	// ------------------------------------------
+	function makeButton(myclass, data){
+		var name=data;
+		name = name.replace(/_/g,' ');
+		name = name.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
+		return '<a href="#" class="'+myclass+'" data-value="'+data+'">'+name+'</a>'+"\n";
+	}
+
+	// handles Image Preview on hover --------------------------------------------------
+	var $container = $('#toh-image-preview');
+	$(document).on({
+		mouseenter: function(e) {
+		var $link = $(this);
+		var imageUrl = $link.attr('href');
+		$container.html('<img src="' + imageUrl + '" alt="Image Preview">');
+		
+		// Wait for the image to load before positioning
+		$container.find('img').on('load', function() {
+			positionPreview($link, $container);
+		});
+
+		$container.show();
+		},
+		mouseleave: function() {
+		$container.hide().empty();
+		}
+	}, 'a.cell-image');
+
+	// Prevent default link behavior
+	//$(document).on('click', 'a.cell-image', function(e) {
+		//e.preventDefault();
+	//});
+
+	// Function to position the preview
+	function positionPreview($link, $container) {
+		var linkOffset = $link.offset();
+		var linkWidth = $link.outerWidth();
+		var linkHeight = $link.outerHeight();
+		var containerWidth = $container.outerWidth();
+		var containerHeight = $container.outerHeight();
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+		var scrollTop = $(window).scrollTop();
+
+		var left = linkOffset.left + linkWidth + 10; // 10px to the right of the link
+		var top = linkOffset.top;
+
+		// Check if the preview would go off the right edge of the window
+		if (left + containerWidth > windowWidth) {
+		left = linkOffset.left - containerWidth - 10; // 10px to the left of the link
+		}
+
+		// Check if the preview would go off the bottom of the viewport
+		if (top + containerHeight > scrollTop + windowHeight) {
+		top = Math.max(scrollTop, top + linkHeight - containerHeight);
+		}
+
+		// Ensure the preview doesn't go above the top of the viewport
+		top = Math.max(scrollTop, top);
+
+		$container.css({
+		left: left,
+		top: top
+		});
+	}
+
+	// ## MAIN #########################################################################
+	// initialize table ################################################################
+	var table = new Tabulator("#table-main", {
+			importFormat:"array",
+			height: "100%",
+			columns:[],
+			//renderHorizontal:"virtual",
+			pagination: true,
+			paginationSize: 50,
+			paginationButtonCount: 10,
+			//	autoColumns:true, 			//create columns from data field names
+			movableColumns:true,      //allow column order to be changed
+			columnDefaults:{
+					headerFilter:true,
+					headerTooltip:true,
+					tooltip:true,         //show tool tips on cells
+			},
+	});
+
+
+	//observe DOM, because i've not found a Tabulator event to do that, ie when changing a large amount of col visibility ---------
+	var observer = new MutationObserver(function(mutations) {
+		// Debounce the callback to avoid multiple triggers
+		clearTimeout(window.domChangeTimer);
+		window.domChangeTimer = setTimeout(function() {
+		// Trigger a custom event when DOM changes are complete
+		$(document).trigger('table-change-complete');
+		}, 100);
+	});
+
+	// Configure the observer
+	var config = { 
+		childList: true, 
+		subtree: true 
+	};
+
+	// Start observing the document body
+	observer.observe(document.getElementById('table-main'), config);
+
+	// Bind to the custom event
+	$(document).on('table-change-complete', function() {
+		console.log('table changes completed');
+		$('#head-loading').hide();
+	});
+	
+	/*
+
+	table.on("redrawStarted", function(){
+		//$('#head-loading').show();
+		console.log('redrawStarted');
+	});	
+	table.on("redrawComplete", function(){
+		//$('#head-loading').hide();
+		console.log('redrawComplete');
+	});	
+	table.on("columnVisibilityChanged", function(){
+		//$('#head-loading').show();
+		console.log('columnVisibilityChanged');
+	});	
+	table.on("renderComplete", function(){
+		//$('#head-loading').hide();
+		console.log('renderComplete');
+	});	
+	table.on("tableRendered", function(){
+		$('#head-loading').hide();
+		console.log('tableRendered');
+	});		
+
+	table.on("tableBuilt", function(){
+		console.log('tableBuilt');
+	});	
+	*/
+
+	// Takes GET parameter of defauls
+	let init_view=getParameterOrDefault('view','normal');
+
+	//make column order
+	$.each(colViewGroups,function(key,obj){
+		$.each(obj.fields,function(f,field){
+			columnOrder.push(field);
+		});
+	});
+
+	//Fetch content and build -----------------------
+
+	$.getJSON( "https://openwrt.org/toh.json", function( data ){ 
+		//Makes columns
+		var columns = data.columns.map((value, index) => ({
+			field: value,
+			title: data.captions[index],
+			visible: false,
+			...columnStyles[value]
+		}));
+
+		//init table with data
+		$('#head-loading').show();
+		table.setColumns(columns);
+		table.setData(data.entries).then(function(){
+			// order columns			
+			columns.sort((a, b) => {
+				const indexA = columnOrder.indexOf(a.field);
+				const indexB = columnOrder.indexOf(b.field);
+				if (indexA === -1 && indexB === -1) return 0; // Both names not in order, keep original order
+				if (indexA === -1) return 1; // a's name not in order, move to end
+				if (indexB === -1) return -1; // b's name not in order, move to end
+				return indexA - indexB;
+			});
+			table.setColumns(columns);
+			//default view
+			//applyView('normal');
+			$("#head-views A[data-value='"+init_view+"']").trigger('click');
+			//groupsUpdateIcon();
+
+			// sort columns						
+			table.setSort([
+				{column:"model", dir:"asc"}, //then sort by this second
+				{column:"brand", dir:"asc"} //sort by this first
+			]);
+			//$('#head-loading').hide();
+		});   
+	});
+
+// formatters ########################################################################
+
+
+// handles presets, filter and views ########################################################################
+
+	// make view custom ----------------
+	var views_html='';
+	views_html+=makeButton('view','all');
+	views_html+=makeButton('view','none');
+	for (const key in colViews){
+		views_html+=makeButton('view',key);
+	}
+	views_html+=makeButton('view','custom');
+	$('#head-views').html(views_html);
+	//groupsUpdateIcon();
+
+	// view presets click ----------------------
+	$('#head-views').on('click','A',function(e){
+		console.log('apply '+$(this).data('value'));
+		applyView($(this).data('value'));
+		$('#head-views A').removeClass('selected');
+		$(this).addClass('selected');
+		groupsUpdateIcon();
+	});
+
+
+	// one view click (or change) ----------------------
+	$('#custom-content').on('click change','INPUT',function(e){
+		var field=$(this).val();
+		if($(this).is(":checked")){
+			table.showColumn(field);
+		}
+		else{
+			table.hideColumn(field);
+		}
+		//$(this).trigger('changed');
+		groupsUpdateIcon();
+	});
+
+	//  view group click ----------------------
+	$('#custom-content').on('click','.group-title A',function(e){
+		$('#head-loading').show();
+		var checked	=$(this).parents('.view-group').find('LI INPUT:checked').length;
+		var inputs	=$(this).parents('.view-group').find('LI INPUT');
+		if(checked==0){
+			inputs.prop('checked', true).trigger('change');
+		}
+		else{
+			inputs.prop('checked', false).trigger('change');
+		}
+		groupsUpdateIcon();
+	});
+
+
+	$('#custom-content').on('changed','INPUT',function(e){
+		//groupsUpdateIcon();
+	});
+		
+
+});
+
+	
